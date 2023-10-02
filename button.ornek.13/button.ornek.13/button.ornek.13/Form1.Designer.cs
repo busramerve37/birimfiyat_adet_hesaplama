dbc7f4e2c7302b725d9a3d97ba82884c13a4fcd1
busramerve37 <145451633+busramerve37@users.noreply.github.com>
2023-10-02 14:24:05 +0300

    Add files via upload

---
+++ button.ornek.13/button.ornek.13/button.ornek.13/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace button.ornek._13
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblToplam = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtBirimfiyat = new System.Windows.Forms.TextBox();
+            this.txtAdet = new System.Windows.Forms.TextBox();
+            this.btnHesapla = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Birim Fiyat:";
+            // 
+            // lblToplam
+            // 
+            this.lblToplam.AutoSize = true;
+            this.lblToplam.Location = new System.Drawing.Point(119, 109);
+            this.lblToplam.Name = "lblToplam";
+            this.lblToplam.Size = new System.Drawing.Size(73, 13);
+            this.lblToplam.TabIndex = 1;
+            this.lblToplam.Text = "Toplam Tutar:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 61);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Adet:";
+            // 
+            // txtBirimfiyat
+            // 
+            this.txtBirimfiyat.Location = new System.Drawing.Point(89, 29);
+            this.txtBirimfiyat.Name = "txtBirimfiyat";
+            this.txtBirimfiyat.Size = new System.Drawing.Size(100, 20);
+            this.txtBirimfiyat.TabIndex = 3;
+            // 
+            // txtAdet
+            // 
+            this.txtAdet.Location = new System.Drawing.Point(89, 61);
+            this.txtAdet.Name = "txtAdet";
+            this.txtAdet.Size = new System.Drawing.Size(100, 20);
+            this.txtAdet.TabIndex = 4;
+            // 
+            // btnHesapla
+            // 
+            this.btnHesapla.Location = new System.Drawing.Point(237, 28);
+            this.btnHesapla.Name = "btnHesapla";
+            this.btnHesapla.Size = new System.Drawing.Size(88, 86);
+            this.btnHesapla.TabIndex = 5;
+            this.btnHesapla.Text = "HESAPLA";
+            this.btnHesapla.UseVisualStyleBackColor = true;
+            this.btnHesapla.Click += new System.EventHandler(this.btnHesapla_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(433, 209);
+            this.Controls.Add(this.btnHesapla);
+            this.Controls.Add(this.txtAdet);
+            this.Controls.Add(this.txtBirimfiyat);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblToplam);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Fiy";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblToplam;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtBirimfiyat;
+        private System.Windows.Forms.TextBox txtAdet;
+        private System.Windows.Forms.Button btnHesapla;
+    }
+}
+
